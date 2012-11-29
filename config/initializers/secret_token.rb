d77433ce48511120dd94428c7855765ecb4acfde
Nikalyukin Mikhail <idups1k@gmail.com>
2012-11-29 12:20:14 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Milavitsa::Application.config.secret_token = '1aa4860b268f584cd6b8704726b82bd1bd44e397d6bc0538ffb1bbeb1aae3d7ff99c5d87fd13fc13ba2cb0be67360b076c533a809f11aa2d3d514ec9cce90326'
