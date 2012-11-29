3fd6e94bedc1269c77186e86c9b627c90566e07a
Nikalyukin Mikhail <idups1k@gmail.com>
2012-11-29 14:35:05 +0200

    remove price TAX feature

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,16 +1,10 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image, :price, :title, :image, :image_cache, :remove_image
 
-  TAX = 1.7
-
   mount_uploader :image, ImageUploader
 
   validates :title, :price, presence: true
 
   before_save :add_tax
-
-  def add_tax
-    self.price = price * Product::TAX
-  end
 
 end
