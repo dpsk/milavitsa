9e7e9612e732d65e0dcc286ac1bf2aceb4c47743
Mikhail Nikalyukin <idups1k@gmail.com>
2013-04-08 13:22:27 +0300

    Update task_scheduler.rb

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -4,7 +4,7 @@ scheduler = Rufus::Scheduler.start_new
 scheduler.every '15m' do
    require "net/http"
    require "uri"
-   urls = ['http://nikalyuk.in', 'http://milavitsa.herokuapp.com']
+   urls = ['http://nikalyuk.in', 'http://milavitsa.herokuapp.com', 'http://infocushipchat.herokuapp.com/']
    urls.each do |url|
      Net::HTTP.get_response(URI.parse(url))
    end
