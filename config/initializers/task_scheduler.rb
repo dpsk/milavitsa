6bcc927a423d07355edf6ecaeea16733589aa03b
Mikhail Nikalyukin <idups1k@gmail.com>
2013-09-18 09:05:17 +0300

    add ping for kindreminders

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -4,7 +4,7 @@ scheduler = Rufus::Scheduler.start_new
 scheduler.every '15m' do
    require "net/http"
    require "uri"
-   urls = ['http://nikalyuk.in', 'http://milavitsa.herokuapp.com', 'http://infocushipchat.herokuapp.com/', 'http://svitla-book-club.herokuapp.com/']
+   urls = ['http://infocushipchat.herokuapp.com/', 'http://kindreminders.herokuapp.com']
    urls.each do |url|
      Net::HTTP.get_response(URI.parse(url))
    end
