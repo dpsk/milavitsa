330e1185fa91919b370401973bc73d888d882dcd
Mikhail Nikalyukin <idups1k@gmail.com>
2013-09-18 09:11:42 +0300

    add self to list

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -4,7 +4,7 @@ scheduler = Rufus::Scheduler.start_new
 scheduler.every '15m' do
    require "net/http"
    require "uri"
-   urls = ['http://infocushipchat.herokuapp.com/', 'http://kindreminders.herokuapp.com']
+   urls = ['http://infocushipchat.herokuapp.com/', 'http://kindreminders.herokuapp.com', , 'http://milavitsa.herokuapp.com',]
    urls.each do |url|
      Net::HTTP.get_response(URI.parse(url))
    end
